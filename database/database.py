0f3f491f786de929007e251a6b872ffae0291031
Th3Mka <164897216+Th3Mka@users.noreply.github.com>
2024-12-18 09:44:28 +0300

    database.py

--- database/database.py
+++ database/database.py
@@ -7,12 +7,29 @@ DATABASE_URL = "sqlite:///checkPriceWB.db"  # Путь к вашей базе д
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
 
+
+class ProductLink(Base):
+    __tablename__ = 'product_links'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer)  # ID пользователя Telegram
+    product_id = Column(Integer)  # ID товара
+    link = Column(String)  # Ссылка на товар
+    name = Column(String)  # Название товара
+    brand = Column(String)  # Бренд товара
+    price = Column(Integer)  # Цена товара в копейках
+    price_with_wallet = Column(Integer)  # Цена с WB Кошельком в копейках
 class Feedback(Base):
     __tablename__ = 'feedback'
 
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer)  # ID пользователя Telegram
     message = Column(String)  # Отзыв пользователя
+
+class Product(Base):
+    __tablename__ = 'products'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer)  # ID пользователя Telegram
+    product_id = Column(Integer)  # ID товара
 
 class User(Base):
     __tablename__ = 'users'
@@ -22,7 +39,11 @@ class User(Base):
     name = Column(String)  # Имя пользователя
     age = Column(Integer)  # Возраст пользователя
 
+
 # Создаем таблицы
 Base.metadata.create_all(engine)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+
