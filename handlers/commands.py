88af8189e91f967cd9f990a10349962e732ced06
Th3Mka <164897216+Th3Mka@users.noreply.github.com>
2024-12-18 09:44:49 +0300

    commands.py

--- handlers/commands.py
+++ handlers/commands.py
@@ -1,4 +1,3 @@
-import requests
 from aiogram import Router, types
 from aiogram.fsm.context import FSMContext
 from database.database import SessionLocal, User
@@ -14,7 +13,7 @@ async def cmd_start(message: types.Message, state: FSMContext):
 
     if existing_user:
         # Если пользователь уже зарегистрирован, показываем инлайн-кнопки
-        await message.answer("Добро пожаловать обратно, {}! Вы уже зарегистрированы.".format(existing_user.name),
+        await message.answer("Добро пожаловать обратно, {}!".format(existing_user.name),
                              reply_markup=create_price_monitoring_keyboard())
     else:
         # Если пользователь не зарегистрирован, запрашиваем имя
@@ -33,63 +32,11 @@ async def cmd_help(message: types.Message):
     await message.answer(help_text, reply_markup=create_price_monitoring_keyboard())
 
 
-@router.message(lambda message: message.text == '/check_price')
-async def cmd_check_price(message: types.Message):
-    # Здесь ваш код для получения цены товара (можно использовать тот же код, что и в обработчике колбэка)
-    params = {
-        'ab_testing': 'false',
-        'appType': '1',
-        'curr': 'rub',
-        'dest': '-5518666',
-        'hide_dtype': '10',
-        'lang': 'ru',
-        'query': 'микрофибра для авто',  # Пример запроса
-        'resultset': 'catalog',
-        'sort': 'popular',
-        'spp': '30',
-    }
-    response = requests.get('https://search.wb.ru/exactmatch/ru/common/v9/search', params=params)
-    if response.status_code == 200:
-        data = response.json()
-        products = data.get('data', {}).get('products', [])
-        if products:
-            first_product = products[0]
-            product_info = {
-                "id": first_product.get("id"),
-                "name": first_product.get("name"),
-                "brand": first_product.get("brand"),
-                "price": first_product['sizes'][0]['price']['total'] / 100,
-                "link": f"https://www.wildberries.ru/catalog/{first_product['id']}/detail.aspx"
-            }
-            response_message = (
-                f"Информация о товаре:\n"
-                f"ID: {product_info['id']}\n"
-                f"Название: {product_info['name']}\n"
-                f"Бренд: {product_info['brand']}\n"
-                f"Цена: {product_info['price']} руб.\n"
-                f"Ссылка на товар: {product_info['link']}"
-            )
-            await message.answer(response_message, reply_markup=create_price_monitoring_keyboard())
-        else:
-            await message.answer("Недостаточно товаров в результате поиска.",
-                                 reply_markup=create_price_monitoring_keyboard())
-    else:
-        await message.answer(f"Ошибка при выполнении запроса: {response.status_code}",
-                             reply_markup=create_price_monitoring_keyboard())
-
-
 @router.message(lambda message: message.text == '/feedback')
 async def cmd_feedback(message: types.Message, state: FSMContext):
     feedback_text = "Пожалуйста, введите ваш отзыв:"
     await message.answer(feedback_text, reply_markup=types.ReplyKeyboardRemove())
     await state.set_state(UserInfoStates.waiting_for_feedback)
-
-
-@router.message(lambda message: message.text == '/settings')
-async def cmd_settings(message: types.Message):
-    settings_text = "Настройки бота будут доступны позже."
-    await message.answer(settings_text, reply_markup=create_price_monitoring_keyboard())
-
 
 @router.message(UserInfoStates.waiting_for_name)
 async def process_name(message: types.Message, state: FSMContext):
